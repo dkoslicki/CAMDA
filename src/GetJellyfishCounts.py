827a37056183a3afc768192862cf68218a544e88
dkoslicki <david.koslicki@math.oregonstate.edu>
2017-05-15 01:01:59 -0700

    typo fix

--- src/GetJellyfishCounts.py
+++ src/GetJellyfishCounts.py
@@ -6,7 +6,7 @@ metagenome_bloom_filter = sys.argv[1]
 count_out_file = os.path.abspath('../data/' + os.path.basename(metagenome_bloom_filter) + '-distinctKmers.txt')
 
 # Get the number of unique k-mers in the metagenome
-cmd = jellyfish_loc + " stats " + count_out_file
+cmd = jellyfish_loc + " stats " + metagenome_bloom_filter
 res = subprocess.check_output(cmd, shell=True)
 num_kmers = int(res.split()[3])
 fid = open(count_out_file, 'w')
